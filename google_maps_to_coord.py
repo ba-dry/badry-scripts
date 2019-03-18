8bf521cf22a62ec4b861fc38d1cfd90e1d046cac
Badry Darkoush <bdarkoush@kudu.com.sa>
2019-03-18 16:16:21 +0300

    Initial commit

---
+++ google_maps_to_coord.py
@@ -0,0 +1,26 @@
+import csv
+import re
+from unshortenit import UnshortenIt
+
+unshortener = UnshortenIt()
+final = open('locations_coordinates.csv', mode='w')
+csv_writer = csv.writer(final, dialect='excel')
+with open('locations_link.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        url = row[1]
+        if len(url) == 31:
+            url = unshortener.unshorten(row[1])
+        if re.search('@(\-?[0-9]+\.[0-9]+),(\-?[0-9]+\.[0-9]+)', url, re.DOTALL):
+            location = re.search('@(\-?[0-9]+\.[0-9]+),(\-?[0-9]+\.[0-9]+)', url, re.DOTALL)
+            latitude  = location.groups()[0]
+            longitude = location.groups()[1]
+            csv_writer.writerow([row[1], latitude, longitude])
+            print(f'Location {row[0]}: latitude={latitude} and longitude = {longitude}')
+        elif re.search('q=(\-?[0-9]+\.[0-9]+),(\-?[0-9]+\.[0-9]+)', url, re.DOTALL):
+            location = re.search('q=(\-?[0-9]+\.[0-9]+),(\-?[0-9]+\.[0-9]+)', url, re.DOTALL)
+            latitude  = location.groups()[0]
+            longitude = location.groups()[1]
+            csv_writer.writerow([row[1], latitude, longitude])
+            print(f'Location {row[0]}: latitude={latitude} and longitude = {longitude}')
+final.close()
