9023543ee04aff816592042f3af25f3b70b64069
Badry Darkoush <bdarkoush@kudu.com.sa>
2019-03-20 10:13:09 +0300

    Updated url length

--- google_maps_to_coord.py
+++ google_maps_to_coord.py
@@ -9,18 +9,18 @@ with open('locations_link.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
         url = row[1]
-        if len(url) == 31:
+        if len(url) == 31 or len(url) == 32:
             url = unshortener.unshorten(row[1])
         if re.search('@(\-?[0-9]+\.[0-9]+),(\-?[0-9]+\.[0-9]+)', url, re.DOTALL):
             location = re.search('@(\-?[0-9]+\.[0-9]+),(\-?[0-9]+\.[0-9]+)', url, re.DOTALL)
             latitude  = location.groups()[0]
             longitude = location.groups()[1]
-            csv_writer.writerow([row[1], latitude, longitude])
+            csv_writer.writerow([row[0], latitude, longitude])
             print(f'Location {row[0]}: latitude={latitude} and longitude = {longitude}')
         elif re.search('q=(\-?[0-9]+\.[0-9]+),(\-?[0-9]+\.[0-9]+)', url, re.DOTALL):
             location = re.search('q=(\-?[0-9]+\.[0-9]+),(\-?[0-9]+\.[0-9]+)', url, re.DOTALL)
             latitude  = location.groups()[0]
             longitude = location.groups()[1]
-            csv_writer.writerow([row[1], latitude, longitude])
+            csv_writer.writerow([row[0], latitude, longitude])
             print(f'Location {row[0]}: latitude={latitude} and longitude = {longitude}')
 final.close()
